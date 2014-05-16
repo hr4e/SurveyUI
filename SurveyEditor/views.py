f757f0607483fa18668e92b9172d53583fc3f1b0
Mark Anthony Serrano <MarkAnthony.Serrano2@gmail.com>
2014-05-15 20:04:53 -0700

    Added Django backend; Added .gitignore; Started SurveyEditor module

---
+++ SurveyEditor/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+
+def index(request):
+    template = loader.get_template('SurveyEditor/index.html')
+    world = "Django World"
+    context = RequestContext(request, {
+        'arb_name': world,
+
+    })
+    return HttpResponse(template.render(context))
