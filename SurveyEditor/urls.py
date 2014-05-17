e3ac7337f4b11b8fe497969278bd3edba23ab33e
Mark Anthony Serrano <MarkAnthony.Serrano2@gmail.com>
2014-05-17 10:43:19 -0700

    Added a URL redirect from /test to /test/home

--- SurveyEditor/urls.py
+++ SurveyEditor/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
-
+from django.views.generic.base import RedirectView
 from SurveyEditor import views
 
 urlpatterns = patterns('',
-    url(r'^home/', views.index, name='index'),
+    url(r'^$', RedirectView.as_view(url='home', permanent=False), name='index'),
+    url(r'^home/', views.index, name='home'),
     url(r'^editor/', views.editor, name='editor')
 )
