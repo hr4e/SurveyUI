395a51814219eacaee8f0100bd111d787e83c3c8
Mark Anthony Serrano <MarkAnthony.Serrano2@gmail.com>
2014-05-18 23:41:39 -0700

    Restructured File Directory; Added angular partials;

--- SurveyEditor/static/js/editorCtrl.js
+++ SurveyEditor/static/js/editorCtrl.js
@@ -1,14 +1,9 @@
-'use strict';
-
-/* Controllers */
-// using_underscores for variable names
-// camelCase for function names
-ques_editor.controller('QuesCtrl', function ($scope) {
+var EditorCtrl = function ($scope) {
 
 	var drawQues = function (layer) {
 		
 		var ques = new Kinetic.Group({
-			draggable: true
+		draggable: true
 		});
 		var x_pos = Math.random() * (500-100);
 		var y_pos = Math.random() * (400-100);
@@ -21,8 +16,8 @@ ques_editor.controller('QuesCtrl', function ($scope) {
 			fill: 'green',
 			stroke: 'black',
 			strokeWidth: 4
-		});
-		var text = new Kinetic.Text({
+			});
+			var text = new Kinetic.Text({
 			x: x_pos + 42,
 			y: y_pos + 32,
 			text: 6,
@@ -36,27 +31,27 @@ ques_editor.controller('QuesCtrl', function ($scope) {
 		// add the shape to the layer
 		$scope.layer.add(ques);
 		$scope.stage.add($scope.layer);
-	}
+	};
 
 	// Methods published to the scope
 	// ==============================
 	$scope.initCanvas = function() {
 		var stage = new Kinetic.Stage({
-			container: 'QuesCanvas',
-			width: 600,
-			height: 400
+		container: 'QuesCanvas',
+		width: 600,
+		height: 400
 		})
 		$scope.stage = stage;
 		var layer = new Kinetic.Layer();
 		$scope.layer = layer;
 
 		stage.add(layer);
-	}
+	};
 
 	$scope.surprise = function() {
 		$scope.msg = $scope.windowWidth;
 		drawQues();
-	}
+	};
 
+};
 
-})
--- SurveyEditor/urls.py
+++ SurveyEditor/urls.py
@@ -1,9 +1,16 @@
-from django.conf.urls import patterns, url
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.conf import settings
+from django.conf.urls import patterns, include, url
+
+from django.views.generic import TemplateView
 from django.views.generic.base import RedirectView
 from SurveyEditor import views
 
 urlpatterns = patterns('',
     url(r'^$', RedirectView.as_view(url='home', permanent=False), name='index'),
-    url(r'^home/', views.index, name='home'),
-    url(r'^editor/', views.editor, name='editor')
+    # Home
+    (r'home/', TemplateView.as_view(template_name='home.html')),
+    (r'editor/', TemplateView.as_view(template_name='editor.html')),
 )
