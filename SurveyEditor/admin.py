0346029254585b92448f64ed171904579774766c
Mark Anthony Serrano <MarkAnthony.Serrano2@gmail.com>
2014-05-23 15:04:36 -0700

    Fixed routes; modelforms

--- SurveyEditor/admin.py
+++ SurveyEditor/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 # Register your models here.
-from SurveyEditor.models import Question
+from SurveyEditor.models import Question, Page, Project
 
 admin.site.register(Question)
+admin.site.register(Page)
+admin.site.register(Project)
--- SurveyEditor/views.py
+++ SurveyEditor/views.py
@@ -1,20 +1,29 @@
 from django.shortcuts import render
 
+from SurveyEditor.models import Question
+from django.forms import ModelForm
+
 # Create your views here.
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
+class QuestionForm(ModelForm):
+    class Meta:
+        model = Question
+        fields = ['questionTag', 'questionText', 'helpText', 'explanation']
+
 def index(request):
-    template = loader.get_template('SurveyEditor/index.html')
-    world = "Django World"
+    template = loader.get_template('home.html')
     context = RequestContext(request, {
+        'yes':'yesssssss',
     })
     return HttpResponse(template.render(context))
 
 
 def editor(request):
-    template = loader.get_template('SurveyEditor/editor.html')
-    world = "Django World"
+    template = loader.get_template('editor.html')
+    form = QuestionForm()
     context = RequestContext(request, {
+        'hum' : form,
     })
     return HttpResponse(template.render(context))
