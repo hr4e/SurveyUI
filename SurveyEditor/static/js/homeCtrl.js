0a590112423d5d760de8e224000fb771be202b29
Graham Greving <ggreving@ucsc.edu>
2014-05-24 22:09:23 -0700

    Home page modals
    Working on creating the edit existing project part as a modal on the
    home page

--- SurveyEditor/static/js/homeCtrl.js
+++ SurveyEditor/static/js/homeCtrl.js
@@ -45,9 +45,49 @@ var ModalInstanceCtrl = function ($scope, $modalInstance, items) {
 };
 
 
+var SelectProjectCtrl = function ($scope, $modal, $log, Nav) {
 
 
+  $scope.items = ['item1', 'item2', 'item3'];
 
+  $scope.open = function (size) {
+
+    var modalInstance = $modal.open({
+      templateUrl: 'selectProjectModal.html',
+      controller: SelectProjectModalInstanceCtrl,
+      size: size,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+};
+
+// Please note that $modalInstance represents a modal window (instance) dependency.
+// It is not the same as the $modal service used above.
+
+var SelectProjectModalInstanceCtrl = function ($scope, $modalInstance, items) {
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $modalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+};
 
 var NewQuestionModalCtrl = function ($scope, $modal, $log) {
 
